f7fc2a67f449fe096f85e21b50f867a0410d3a33
”RECRay” <recray@mail.ru>
2023-10-03 10:32:27 +0700

    DB connection

---
+++ src/main/java/DBConnection/DatabaseConnection.java
@@ -0,0 +1,47 @@
+package DBConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String DRIVER = "org.postgresql.Driver";
+    private static final String URL = "jdbc:postgresql://localhost:5432/shop";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "12345";
+
+    private static volatile DatabaseConnection instance;
+    private Connection connection;
+
+    // Приватный конструктор для предотвращения создания экземпляров снаружи
+    private DatabaseConnection() {
+        initializeConnection();
+    }
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            synchronized (DatabaseConnection.class) {
+                if (instance == null) {
+                    instance = new DatabaseConnection();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    public void initializeConnection() {
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Класс org.postgresql.Driver не найден");
+        } catch (SQLException e) {
+            System.out.println("Не удалось подключиться к базе данных");
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import DBConnection.DatabaseConnection;
+
+import java.awt.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
